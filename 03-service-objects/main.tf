636a4e91ed33cea231dc1f9a78b5292735805994
kym.hughes@microsoft.com <kym.hughes@microsoft.com>
2026-08-25 21:11:19 +1000

    y

--- 03-service-objects/main.tf
+++ 03-service-objects/main.tf
@@ -9,11 +9,6 @@ resource "azurerm_resource_group" "service_objects" {
 
 data "azurerm_client_config" "current" {}
 
-# data "azuread_group" "avd_users" {
-#   display_name     = var.user_group_name
-#   security_enabled = true
-# }
-
 resource "random_string" "suffix" {
   length  = 4
   special = false
@@ -95,3 +90,9 @@ resource "azurerm_role_assignment" "avd_network_contributor" {
   principal_id                     = var.avd_service_principal_object_id
   skip_service_principal_aad_check = true
 }
+
+
+# data "azuread_group" "avd_users" {
+#   display_name     = var.user_group_name
+#   security_enabled = true
+# }
