a633d14ca119e5e1fd29ca91eeeaeec98f07ca50
kym.hughes@microsoft.com <kym.hughes@microsoft.com>
2026-08-28 18:53:41 +1000

    y

---
+++ 11-ref-image-capture/2.Capture_Image_Version.tf
@@ -0,0 +1,15 @@
+resource "azurerm_shared_image_version" "capture" {
+  name                = var.version
+  gallery_name        = var.gallery_name
+  image_name          = var.image_name
+  resource_group_name = var.gallery_rg
+  location            = var.location
+
+  managed_image_id = azurerm_image.capture.id
+
+  target_region {
+    name                   = "australiaeast"
+    regional_replica_count = 2
+  }
+}
+``
