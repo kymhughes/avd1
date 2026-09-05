4f87972054893e1d2e55fbe13ce7418725510658
kym.hughes@microsoft.com <kym.hughes@microsoft.com>
2026-09-05 19:05:15 +1000

    y

--- 07-imagebuilder/main.tf
+++ 07-imagebuilder/main.tf
@@ -108,10 +108,9 @@ resource "azurerm_shared_image" "aib" {
 }
 
 resource "azurerm_resource_group_template_deployment" "aib" {
-  name                                = var.image_template_name
-  resource_group_name                 = azurerm_resource_group.aib.name
-  deployment_mode                     = "Incremental"
-  delete_nested_items_during_deletion = false
+  name                = var.image_template_name
+  resource_group_name = azurerm_resource_group.aib.name
+  deployment_mode     = "Incremental"
   parameters_content = jsonencode({
     "imageTemplateName" = {
       value = var.image_template_name
